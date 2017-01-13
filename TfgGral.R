711e9d0dfd59833283845880247a22723a2d654f
jorsugar <jodsua@gmail.com>
2017-01-13 10:51:03 -0300

    Mejoras en variable Fecha de datosGenerales

--- TfgGral.R
+++ TfgGral.R
@@ -87,6 +87,8 @@ write.csv(gral, "datosGenerales.csv")
 
 #---------------------------------------
 
+#Los nombres de los docentes aparecen en mayúscula en el SIFP. Se arregla este aspecto en este apartado.
+
 gral1 <- read.csv("datosGenerales.csv")
 
 
@@ -109,6 +111,23 @@ View(gral1)
 write.csv(gral1, "datosGenerales.csv")
 
 
+#--------------------------------------------------
+#Mejora variable Fecha
+#La variable fecha incluye la hora de lectura del TFG. Esa hora no es relevante por lo que hay que sacarla.
 
+gral2 <- read.csv("datosGenerales.csv")
 
+library(dplyr)
+library(stringr)
 
+x <- gral2$Fecha
+x <-  str_sub(x, start=1, end=10)
+
+gral2 <- select(gral2, -X, -Fecha)                                      #elimino la columna fecha que corregí
+
+Fecha <- x
+
+gral2 <- mutate(gral2, Fecha)                                           #agrego el vector con la fecha nueva como columna
+gral2 <- select(gral2, Título, Fecha, Formato, Tutor, Revisor, Nota)    #reordeno las variables  
+
+write.csv(gral2, "datosGenerales.csv")
