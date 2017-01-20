bbfbc3669704afe9d391423acb77895ee336e4f5
jorsugar <jodsua@gmail.com>
2017-01-20 18:23:42 -0300

    Se agregan ID docentes en datosGrales - TfgGral

--- TfgGral.R
+++ TfgGral.R
@@ -46,6 +46,7 @@ nombre <- Url %>%
 length(nombre)
 nombre <- nombre[2:1319]
 
+
 apellido <- Url %>%
   html_nodes(".views-field-field-apellido-pdocente") %>%
   html_text(trim= TRUE)
@@ -131,3 +132,53 @@ gral2 <- mutate(gral2, Fecha)                                           #agrego 
 gral2 <- select(gral2, Título, Fecha, Formato, Tutor, Revisor, Nota)    #reordeno las variables  
 
 write.csv(gral2, "datosGenerales.csv")
+
+#--------------------------------------------------------
+#Se crean dos nuevas variables con los ID de los docentes. Esto permitirá matchear por docente 
+#de forma más fácil cuando sea necesario. Los ID son los que utiliza el SIFP
+
+library(xml2)
+library(rvest)
+library(dplyr)
+library(stringr)
+
+Url <- read_html("http://sifp1.psico.edu.uy/trabajos-finales-publicos")
+
+idNombre <- Url %>%
+  html_nodes(".views-field-field-nombre-pdocente a") %>%
+  html_attr(name="href")
+
+length(idNombre)
+
+cuatroF <- function(x){substr(x, nchar(x)-4+1, nchar(x))}
+
+idNombre <- cuatroF(idNombre)
+idTutor <- idNombre
+rm (idNombre)
+
+
+idNombrer <- Url %>%
+  html_nodes(".views-field-field-nombre-pdocente-1 a") %>%
+  html_attr(name="href")
+
+length(idNombrer)
+
+idNombrer <- cuatroF(idNombrer)
+idRevisor <- idNombrer
+rm (idNombrer)
+idRevisor <- append(idRevisor, c("1", "2", "3", "4") , after = length(idRevisor)) #se agregan cuatro elementos al vector para poder agregarlo la base general.
+
+
+gral3 <- read.csv("datosGenerales.csv")
+gral3 <- mutate(gral3, idTutor, idRevisor)
+gral3 <- select(gral3, -X, Título, Fecha, Formato, idTutor, Tutor, idRevisor, Revisor, Nota)
+gral3 <- select(gral3, Título, Fecha, Formato, idTutor, Tutor, idRevisor, Revisor, Nota)
+
+write.csv(gral3, "datosGenerales.csv")
+
+
+
+
+
+
+
